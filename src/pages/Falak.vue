737db30466c1193aedcfa724d005462a8fc2ecf4
A. Y. Firdaus <amyar32@gmail.com>
2021-11-26 13:45:46 +0700

    new route

---
+++ src/pages/Falak.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="p-5">
+    <div class="text-center">
+      <div class="mb-16 space-y-3">
+        <h1 class="text-white font-black text-4xl sm:text-5xl md:text-7xl">
+          ILMU FALAK
+        </h1>
+        <p class="text-gray-100 text-sm md:text-xl">
+          Berikut link untuk mengakses materi berbentuk slide
+        </p>
+      </div>
+      <div class="space-x-2">
+        <a
+          class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          href="https://drive.google.com/drive/folders/1a7MCxAhYVXcl2lgxjBsAfxWJWw-Fka3J?usp=sharing"
+        >
+          Google Drive
+        </a>
+        <a
+          class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          href="https://1drv.ms/u/s!ApsjFHoaKkB_gsB8ZXn4qDVO0kdUKQ?e=pAPEZ9"
+        >
+          One Drive
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from "../pages/Home.vue";
 import Experience from "../pages/Experience.vue";
 import Education from "../pages/Education.vue";
 import Blog from "../pages/Blog.vue";
+import Falak from "../pages/Falak.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -26,6 +27,11 @@ const router = createRouter({
       name: "Blog",
       path: "/blog",
       component: Blog,
+    },
+    {
+      name: "Falak",
+      path: "/ilmufalak",
+      component: Falak,
     },
   ],
 });
